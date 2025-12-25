7c0835908afcbdd243eb9835e4d34d1b77eb1a5f
suhatthya <s6703052411376@email.kmutnb.ac.th>
2025-12-25 11:40:28 +0700

    Date

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Content, Footer, Header } from "./func-components";
+import Calendar from "./class-components";
 
 
 function App() {
   return (
-    [<Header/>,<Content/>,<Footer/>]
+    <>
+    <Header/>
+    <p><center><Calendar/></center></p>
+    <Content/>
+    <Footer/>
+    </>
   )
 }
 
