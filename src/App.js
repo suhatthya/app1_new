eb5e18c3d2056c672af4e6f639bce97ddf50eece
suhatthya <s6703052411376@email.kmutnb.ac.th>
2026-01-15 11:17:35 +0700

    State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import RefsArray from './refs-array'
+import MessageBox from './state-func'
+
 
 export default function App() {
-  return <RefsArray/>
+  return <MessageBox/>
 }
