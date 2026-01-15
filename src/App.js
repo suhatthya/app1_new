40a98f6bacff06e06b3b5e40f972be37f46f886c
suhatthya <s6703052411376@email.kmutnb.ac.th>
2026-01-15 10:28:02 +0700

    References

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { EventData2 } from './event-data'
+import RefsArray from './refs-array'
 
 export default function App() {
-  return <EventData2/>
+  return <RefsArray/>
 }
